ff9dde7c7add7edec77d9cf71e5d654e290cb224
Your Name <pttanuradha@std.appsc.sab.ac.lk>
2022-04-23 00:20:34 +0530

    ...

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $('menu').click(function(){
         $(this).toggleclass('toggle');
     });
+    $(window).on('scroll load', function(){
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+    });
 });
