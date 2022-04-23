9c6ae565ed40ee6ecf2f6feb98284b5a9240535d
Your Name <pttanuradha@std.appsc.sab.ac.lk>
2022-04-23 12:02:53 +0530

    end commit

--- script.js
+++ script.js
@@ -6,4 +6,17 @@ $(document).ready(function(){
         $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle');
     });
+    //smooth scrolling
+
+    $('a[href*="#"]').on('click',function(e){
+        e.preventdefault();
+        $('html,body').animate({
+
+            scrollTop : $($(this).attr('href')).offset().top,
+
+        },
+        500,
+        'linear'
+        );
+    });
 });
